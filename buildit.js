7470a4e02515946bef084893b71182d66a52cedd
chris <cryptoskillz@chriss-MacBook-Air.local>
2024-08-07 00:22:15 +0700

    fixed page rendering

--- buildit.js
+++ buildit.js
@@ -25,15 +25,13 @@ if (fs.existsSync("./_data/api.js")) {
     try {
       getapiData = require("./_data/api.js");
       apiData = await getapiData(); // Call getapiData asynchronously
-      console.log(apiData);
-      processTemplates(); // Proceed with template processing only after data is fetched
     } catch (error) {
       console.error("Error fetching API data:", error);
     }
   })();
-} else {
-  processTemplates();
 }
+// Proceed with template processing
+processTemplates();
 
 function processTemplates() {
   const sourceFolder = "./_source";
